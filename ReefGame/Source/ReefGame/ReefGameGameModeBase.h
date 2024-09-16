f72b7bf46f1c36bb2c777fff977bb2b451b23374
Joao Miranda <50178167+joaopcmiranda@users.noreply.github.com>
2024-09-16 15:41:08 +1000

    Initial Project

---
+++ ReefGame/Source/ReefGame/ReefGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ReefGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REEFGAME_API AReefGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
